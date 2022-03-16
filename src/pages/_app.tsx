9389147f954fd35e3118dfa8c6c50673ef3f2821
hanana1253 <hanana1253@gmail.com>
2022-03-16 16:49:13 +0900

    chore: 컴포넌트 index 파일 경로 추가, Layout 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,15 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from 'theme/theme';
 import { GlobalStyle } from 'styles/GlobalStyle';
+import { Layout } from '../components';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ThemeProvider>
   );
 }
