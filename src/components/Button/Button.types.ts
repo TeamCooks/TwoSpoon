9768edc0d7af6080d60b6de3be2b05fab6c968b3
hhhyyo <gydnjsrh6923@korea.ac.kr>
2022-03-17 18:50:18 +0900

    feat: Add IconButtonProps type - Issue #51

--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -1,6 +1,8 @@
 type Type = 'button' | 'submit';
 type Variant = 'transparent' | 'outlined' | 'filled';
 type Color = 'primaryGreen' | 'primaryOrange' | 'white';
+type Icon = 'search' | 'user' | 'heart' | 'close' | 'cart' | 'link' | 'bookmark' | 'bookmarkFill' | 'up';
+type Size = 'small' | 'large';
 
 export interface StyledButtonProps {
   type: Type;
@@ -13,4 +15,14 @@ export interface StyledButtonProps {
 export interface ButtonProps extends StyledButtonProps {
   variant: Variant;
   children: React.ReactNode;
+}
+
+export interface IconButtonProps {
+  type: Type;
+  ariaLabel: string;
+  icon: Icon;
+  circle: boolean;
+  variant: Variant;
+  color: Color;
+  size: Size;
 }
