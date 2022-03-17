6fa9a49611132a04024950e525728427cf36d61b
hhhyyo <gydnjsrh6923@korea.ac.kr>
2022-03-17 19:49:42 +0900

    feat: Create IconButton component - Issue #51

---
+++ src/components/Button/IconButton.tsx
@@ -0,0 +1,72 @@
+import { FiSearch } from 'react-icons/fi';
+import { FaArrowUp } from 'react-icons/fa';
+import { BsPersonCircle, BsCartCheckFill, BsLink45Deg, BsBookmark, BsFillBookmarkFill } from 'react-icons/bs';
+import { HiHeart } from 'react-icons/hi';
+import { MdClose } from 'react-icons/md';
+import { StyledButton, StyledOutlineButton, StyledFillButton } from './IconButton.styled';
+import { IconButtonProps, StyledIconButtonProps } from './Button.types';
+
+const renderIcon = (iconType: IconButtonProps['iconType']) => {
+  switch (iconType) {
+    case 'search':
+      return <FiSearch />;
+    case 'user':
+      return <BsPersonCircle />;
+    case 'heart':
+      return <HiHeart />;
+    case 'close':
+      return <MdClose />;
+    case 'cart':
+      return <BsCartCheckFill />;
+    case 'link':
+      return <BsLink45Deg />;
+    case 'bookmarkFill':
+      return <BsFillBookmarkFill />;
+    case 'bookmark':
+      return <BsBookmark />;
+    case 'up':
+      return <FaArrowUp />;
+    default:
+      return null;
+  }
+};
+
+const renderButton = ({
+  type,
+  ariaLabel,
+  iconType,
+  circle = false,
+  variant,
+  color,
+  size,
+  ...restProps
+}: IconButtonProps) => {
+  const props: StyledIconButtonProps = { type, ariaLabel, circle, color, size };
+
+  switch (variant) {
+    case 'transparent':
+      return (
+        <StyledButton {...props} {...restProps}>
+          {renderIcon(iconType)}
+        </StyledButton>
+      );
+    case 'outlined':
+      return (
+        <StyledOutlineButton {...props} {...restProps}>
+          {renderIcon(iconType)}
+        </StyledOutlineButton>
+      );
+    case 'filled':
+      return (
+        <StyledFillButton {...props} {...restProps}>
+          {renderIcon(iconType)}
+        </StyledFillButton>
+      );
+    default:
+      return null;
+  }
+};
+
+export const IconButton = (props: IconButtonProps) => {
+  return renderButton(props);
+};
