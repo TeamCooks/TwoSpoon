f83781babf753f2d01694639adb2c45dd936f387
hanana1253 <hanana1253@gmail.com>
2022-03-18 13:46:15 +0900

    fix: Import Path 잘못 표기한 story 파일 수정
    - default export를 수정하면서 경로 변경되어 story파일을 수정했습니다.

--- src/components/EmptyPage/EmptyPage.stories.tsx
+++ src/components/EmptyPage/EmptyPage.stories.tsx
@@ -3,24 +3,28 @@ import Link from 'next/link';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from 'theme/theme';
 import { GlobalStyle } from 'styles/GlobalStyle';
-import EmptyPage from './EmptyPage';
+import { EmptyPage } from './EmptyPage';
 
 export default {
   title: 'EmptyPage',
   component: EmptyPage,
   args: {
-    children: <><h2>Headline</h2><p>Description</p></>
+    children: (
+      <>
+        <h2>Headline</h2>
+        <p>Description</p>
+      </>
+    ),
   },
   decorators: [
     (Story) => (
       <ThemeProvider theme={theme}>
-          <GlobalStyle />
-          <Story />
+        <GlobalStyle />
+        <Story />
       </ThemeProvider>
     ),
-  ]
+  ],
 } as ComponentMeta<typeof EmptyPage>;
-
 
 const Template: ComponentStory<typeof EmptyPage> = (args) => <EmptyPage {...args} />;
 
@@ -29,5 +33,10 @@ export const DefaultEmptyPage = Template.bind({});
 export const ErrorPage = Template.bind({});
 
 ErrorPage.args = {
-  children: <><h2>Oops, something went wrong</h2><Link href='/'>Go Back to main page?</Link></>
-}
+  children: (
+    <>
+      <h2>Oops, something went wrong</h2>
+      <Link href="/">Go Back to main page?</Link>
+    </>
+  ),
+};
