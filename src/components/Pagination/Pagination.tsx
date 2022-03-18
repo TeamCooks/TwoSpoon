e65afb8937ea65d0e2db42909ffcfd8166e9aa59
hanana1253 <hanana1253@gmail.com>
2022-03-18 17:11:56 +0900

    feat: Pagination 컴포넌트 작성
    - className 프롭은 emotion을 사용하므로 더이상 필요없게 되어 삭제
    - 앞 페이지 목록으로 넘어가는 ellipsis 버튼이 딱히 사용성이 있게 느껴지지 않아 삭제

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,52 @@
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
+import { PaginationProps } from './Pagination.styled';
+import { StyledPageButton, StyledPaginationControl } from './Pagination.types';
+
+export const Pagination = ({ limit, currentPage, onClick: handleClick, totalResults }: PaginationProps) => {
+  const lastPageNum = Math.ceil(totalResults / limit);
+  const pageStartNum = Math.max(currentPage - 2, 1);
+  const pageEndNum = Math.min(currentPage + 2, lastPageNum);
+  return (
+    <StyledPaginationControl>
+      <StyledPageButton
+        type="button"
+        aria-label="Go to previous results page."
+        onClick={() => {
+          if (currentPage === 1) return;
+          handleClick(currentPage - 1);
+        }}
+      >
+        <IoIosArrowBack />
+      </StyledPageButton>
+      <ul>
+        {Array.from({ length: pageEndNum - pageStartNum + 1 }, (_, index) => {
+          return (
+            <li key={index}>
+              <StyledPageButton
+                type="button"
+                current={currentPage === pageStartNum + index}
+                aria-pressed={currentPage === pageStartNum + index}
+                aria-label={`Go to page ${pageStartNum + index}`}
+                onClick={() => {
+                  handleClick(pageStartNum + index);
+                }}
+              >
+                {pageStartNum + index}
+              </StyledPageButton>
+            </li>
+          );
+        })}
+      </ul>
+      <StyledPageButton
+        type="button"
+        aria-label="Go to next results page."
+        onClick={() => {
+          if (currentPage > Math.floor(totalResults / limit)) return;
+          handleClick(currentPage + 1);
+        }}
+      >
+        <IoIosArrowForward />
+      </StyledPageButton>
+    </StyledPaginationControl>
+  );
+};
