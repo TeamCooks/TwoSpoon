7e798713e3afd0b381731f6528a7321cd6105b23
hustle-dev <dlwoabsdk@gmail.com>
2022-03-23 15:55:05 +0900

    feat: 스켈레톤 컴포넌트를 위한 타입 추가 - Issue #15

--- src/components/SkeletonCard/SkeletonCard.types.ts
+++ src/components/SkeletonCard/SkeletonCard.types.ts
@@ -8,3 +8,20 @@ export interface SkeletonCardProps {
   hasSummary: boolean;
   headingPosition: SkeletonHeadingPosition;
 }
+
+export interface SkeletonTypeProps {
+  type: SkeletonType;
+}
+
+export interface SkeletonWrapperProps {
+  type: SkeletonType;
+  background: SkeletonBackground;
+}
+
+export interface SkeletonTitleProps {
+  headingPosition: SkeletonHeadingPosition;
+}
+
+export interface SkeletonSummaryProps {
+  hasSummary: boolean;
+}
