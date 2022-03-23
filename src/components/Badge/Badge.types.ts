e5f2ce9e90eaedbe665e24209a37c77e99749376
hhhyyo <gydnjsrh6923@korea.ac.kr>
2022-03-24 01:20:14 +0900

    feat: Add Badge.types.ts - Issue #24

---
+++ src/components/Badge/Badge.types.ts
@@ -0,0 +1,48 @@
+type IconType =
+  | 'dairyFree'
+  | 'lactoOvo'
+  | 'lactoOvoVegetarian'
+  | 'vegetarian'
+  | 'vegan'
+  | 'glutenFree'
+  | 'popular'
+  | 'paleolithic'
+  | 'paleo'
+  | 'primal'
+  | 'healthy'
+  | 'pescatarian';
+
+type Size = 'small' | 'large';
+
+type Text =
+  | 'dairy-free'
+  | 'lacto-ovo'
+  | 'lacto-ovo'
+  | 'vegetarian'
+  | 'vegan'
+  | 'gluten-free'
+  | 'popular'
+  | 'paleo'
+  | 'paleo'
+  | 'primal'
+  | 'healthy'
+  | 'pescatarian';
+
+type Color = 'badgePink' | 'badgePurple' | 'badgeGreen' | 'badgeNavy';
+
+export interface BadgeProps {
+  iconType: IconType;
+  size: Size;
+}
+
+export interface StyledBadgeProps {
+  $color: Color;
+}
+
+export type BadgeInfos = {
+  [key in IconType]: {
+    icon: JSX.Element;
+    text: Text;
+    color: Color;
+  };
+};
