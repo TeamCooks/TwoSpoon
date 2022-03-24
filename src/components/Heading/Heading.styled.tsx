1db0653c78e2e7a2d18d97d17b99ad5bf5da2f57
jhyj0521 <jhyj3636@gmail.com>
2022-03-24 15:40:15 +0900

    fix: Styled component에서 emotion으로 css 방식 변경에 따른 코드 수정

--- src/components/Heading/Heading.styled.tsx
+++ src/components/Heading/Heading.styled.tsx
@@ -1,13 +1,13 @@
-import styled from 'styled-components';
-import { pxToRem } from 'utils';
+import styled from '@emotion/styled';
+import { pxToRem, media } from 'utils';
 
-export const StyledHeading = styled(({ as }) => as)`
+export const StyledHeading = styled.h1`
   font-size: ${pxToRem(32)};
   color: ${({ theme }) => theme.color.primaryOrange};
   font-weight: bold;
   padding-top: ${pxToRem(16)};
 
-  @media ${({ theme }) => theme.break.mobile} {
+  ${media.mobile} {
     font-size: ${pxToRem(26)};
   }
 `;
