389f2ad7af86df41621ce4b67e248aaa52d0c18e
hustle-dev <dlwoabsdk@gmail.com>
2022-03-24 15:17:51 +0900

    feat: Card 스타일링 - Issue #15

--- src/components/Card/Card.styled.tsx
+++ src/components/Card/Card.styled.tsx
@@ -1,164 +1,163 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
-import { pxToRem } from 'utils';
-import { CardContainerProps } from './Card.types';
+import { media, pxToRem } from 'utils';
+import { CardContainerProps, CardFigcaptionProps, CardFigureImgProps } from './Card.types';
+
+const inlineBlock = css`
+  text-align: center;
+  width: 100%;
+  ${media.desktop} {
+    display: inline-block;
+  }
+`;
+
+const wide = css`
+  width: 100%;
+  height: 50vw;
+  object-fit: cover;
+
+  ${media.desktop} {
+    width: 100%;
+    height: 250px;
+  }
+`;
+
+const square = css`
+  width: 100%;
+
+  ${media.mobile} {
+    height: 50vw;
+  }
+  ${media.desktop} {
+    height: rem(200px);
+    // 2행
+    @media (max-width: 1547px) {
+      height: 20vw;
+    }
+
+    @media (max-width: 1100px) {
+      height: 25vw;
+    }
+  }
+  object-fit: cover;
+`;
+
+const smallSquare = css`
+  width: 100%;
+  aspect-ratio: 1 / 1;
+  object-fit: cover;
+  ${media.mobile} {
+    aspect-ratio: 16 / 9;
+  }
+
+  @media (max-width: 1056px) {
+    aspect-ratio: 16 / 9;
+  }
+`;
+
+const bottomLeft = css`
+  font-size: ${pxToRem(20)};
+  left: 0;
+`;
+
+const bottomCenter = css`
+  font-size: ${pxToRem(20)};
+  text-align: center;
+`;
+const topLeft = css`
+  font-size: ${pxToRem(24)};
+  color: orange;
+  order: -1;
+`;
+
+export const CardLink = styled.a`
+  width: 100%;
+  border: none;
+  background-color: transparent;
+  text-align: left;
+  ${media.mobile} {
+    width: 100%;
+  }
+`;
 
 export const CardContainer = styled.div<CardContainerProps>`
-  ${(props) => props.$background === 'white'}
-
   height: 100%;
   display: flex;
   flex-direction: column;
   padding: ${pxToRem(16)};
   position: relative;
   cursor: pointer;
+
+  background: ${(props) => (props.$background === 'white' ? 'white' : 'none')};
+  box-shadow: ${(props) => props.$background === 'white' && '0px 4px 4px rgba(0, 0, 0, 0.25);'};
+
+  ${(props) => props.$type === 'square' && inlineBlock}
 `;
 
-// .wide {
-//     width: 100%;
-//     height: 50vw;
-//     object-fit: cover;
+export const CardFigureImg = styled.img<CardFigureImgProps>`
+  ${(props) => (props.$type === 'wide' ? wide : props.$type === 'square' ? square : smallSquare)}
+`;
 
-//     @include desktop {
-//       width: 100%;
-//       height: 250px;
-//     }
-//   }
+export const CardFigcaption = styled.figcaption<CardFigcaptionProps>`
+  font-size: ${pxToRem(18)};
+  font-weight: bold;
+  padding: ${pxToRem(16)} 0;
+  width: 100%;
 
-//   .square {
-//     width: 100%;
+  ${(props) =>
+    props.$headingPosition === 'topLeft'
+      ? topLeft
+      : props.$headingPosition === 'bottomLeft'
+      ? bottomLeft
+      : bottomCenter}
+`;
 
-//     @include mobile {
-//       height: 50vw;
-//     }
-//     @include desktop {
-//       height: rem(200px);
-//       // 2행
-//       @media (max-width: 1547px) {
-//         height: 20vw;
-//       }
+export const CardSummary = styled.div`
+  line-height: ${pxToRem(24)};
+  ${media.desktop} {
+    display: -webkit-box;
+    word-wrap: break-word;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    &:after {
+      content: '';
+      color: black;
+      position: absolute;
+      display: block;
+      width: 100%;
+      height: 30%;
+      bottom: 0;
+      left: 0;
+      background: linear-gradient(to top, #fff 50%, transparent);
+    }
+  }
+  ${media.mobile} {
+    display: -webkit-box;
+    -webkit-line-clamp: 1;
+    word-wrap: break-word;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
 
-//       @media (max-width: 1100px) {
-//         height: 25vw;
-//       }
-//     }
-//     object-fit: cover;
-//   }
+  margin-top: 0;
+  margin-bottom: ${pxToRem(8)};
+`;
 
-//   .smallSquare {
-//     width: 100%;
-//     aspect-ratio: 1 / 1;
-//     object-fit: cover;
-//     @include mobile {
-//       aspect-ratio: 16 / 9;
-//     }
+export const CardButton = styled.button`
+  ${media.mobile} {
+    display: none;
+  }
 
-//     @media (max-width: 1056px) {
-//       aspect-ratio: 16 / 9;
-//     }
-//   }
+  padding: ${pxToRem(8)} 0;
+  font-size: ${pxToRem(24)};
+  z-index: 1;
+  margin: 0;
 
-//   .cardButton {
-//     width: 100%;
-//     border: none;
-//     background-color: transparent;
-//     text-align: left;
-//     @include mobile {
-//       width: 100%;
-//     }
-//   }
-
-//   .inlineBlock {
-//     text-align: center;
-//     width: 100%;
-//     @include desktop {
-//       display: inline-block;
-//     }
-//   }
-
-//   .white {
-//     background: white;
-//     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-//   }
-
-//   .none {
-//     background: none;
-//   }
-
-//   .title {
-//     font-size: rem(18px);
-//     font-weight: bold;
-//     padding: rem(16px) 0;
-//     width: 100%;
-//   }
-
-//   // hasSummary
-//   .summary {
-//     line-height: rem(24px);
-//     @include desktop {
-//       display: -webkit-box;
-//       word-wrap: break-word;
-//       -webkit-box-orient: vertical;
-//       overflow: hidden;
-//       text-overflow: ellipsis;
-//       &:after {
-//         content: '';
-//         color: black;
-//         position: absolute;
-//         display: block;
-//         width: 100%;
-//         height: 30%;
-//         bottom: 0;
-//         left: 0;
-//         background: linear-gradient(to top, #fff 50%, transparent);
-//       }
-//     }
-//     @include mobile {
-//       display: -webkit-box;
-//       -webkit-line-clamp: 1; /* 라인수 */
-//       word-wrap: break-word;
-//       -webkit-box-orient: vertical;
-//       overflow: hidden;
-//       text-overflow: ellipsis;
-//     }
-//   }
-
-//   // headingPosition
-//   .bottom {
-//     font-size: rem(20px);
-//     &Left {
-//       left: 0;
-//     }
-//     &Center {
-//       text-align: center;
-//     }
-//   }
-
-//   .topLeft {
-//     font-size: rem(24px);
-//     color: orange;
-//     order: -1;
-//   }
-
-//   .more {
-//     @include mobile {
-//       display: none;
-//     }
-//     // border: 1px solid linear-gradient(to right, gray, $primary_green);
-//     // background: linear-gradient(to right, #d4fc79, #96e6a1);
-//     padding: rem(8px) 0;
-//     // text-align: center;
-//     font-size: rem(24px);
-//     z-index: 1;
-//     margin: 0;
-
-//     display: flex;
-//     gap: 10px;
-//     border: none;
-//     background: none;
-//     justify-content: center;
-//   }
-
-//   .text {
-//     margin-top: 0;
-//     margin-bottom: rem(8px);
-//   }
+  display: flex;
+  gap: 10px;
+  border: none;
+  background: none;
+  justify-content: center;
+`;
