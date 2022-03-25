153c9ef644fb794794612b64ffe9756119039c06
hanana1253 <hanana1253@gmail.com>
2022-03-25 12:50:26 +0900

    feat: Loading과 Portal을 위한 div 요소 추가

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+import { useContext } from 'react';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <div id="loading-start" aria-live="assertive"></div>
+        <div id="loading-end" aria-live="assertive"></div>
+        <div id="dialog"></div>
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
