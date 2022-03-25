f288601c181596598d9c1cf1991fba690fc7ab38
hanana1253 <hanana1253@gmail.com>
2022-03-25 23:56:23 +0900

    chore: Dialog 컴포넌트 스타일링 완성
    - dialog 컴포넌트의 스타일링을 완성했습니다.

--- src/components/Dialog/Dialog.styled.tsx
+++ src/components/Dialog/Dialog.styled.tsx
@@ -1,31 +1,39 @@
 import styled from '@emotion/styled';
 import { IconButton } from 'components';
+import { media } from 'utils';
 
 export const StyledDialogContainer = styled.div`
   z-index: 200;
-  position: fixed;
-  top: 0;
-  width: 90%;
-  height: 90%;
+  position: absolute;
+  top: 50vh;
+  left: 50%;
+  ${media.desktop} {
+    width: 60vw;
+  }
+  ${media.mobile} {
+    width: 90vw;
+  }
+  min-height: 50vh;
+  max-height: 80vh;
+  transform: translate(-50%, -50%);
   overflow: auto;
-`;
+  background-color: rgba(0, 0, 0, 0.5);
+  `;
 
 export const StyledDialogContent = styled.div`
   z-index: 200;
-  color: #121212;
-  background: rgba(36, 36, 36, 0.8);
-  backdrop-filter: blur(3px);
-  min-height: 100%;
+  padding: 20px;
+  margin: 20px;
 `;
 
 export const StyledCloseButton = styled(IconButton)`
   cursor: pointer;
   position: absolute;
   z-index: 200;
-  top: 20px;
-  right: 20px;
+  top: 5px;
+  right: 5px;
   border: 0;
-  padding: 10px;
+  padding: 5px;
   background: transparent;
   color: #fefefe;
   svg {
