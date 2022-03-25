c2f7dce18e1f0a42cffbb5cb664c84986bf0809e
hanana1253 <hanana1253@gmail.com>
2022-03-25 13:51:14 +0900

    chore: Auth Component 스타일링 추가
    - auth 컴포넌트에 button 조립 등 스타일링 추가했습니다..

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,35 +1,40 @@
 import { FormikProps, withFormik } from 'formik';
 import { FormValues, FormProps } from './Auth.types';
-import { StyledInput, StyledFieldError } from './Auth.styled';
+import { StyledForm, StyledInput, StyledFieldError, StyledAuthContainer } from './Auth.styled';
 import { AUTH_FUNC, SCHEMA, INITIAL_VALUES, FIELDS, HEADING, PLACEHOLDER, TYPE } from './AuthServices';
+import { Button } from 'components/Button/Button';
 
 const AuthForm = (props: FormProps & FormikProps<FormValues>): JSX.Element => {
   const { currentForm, values, errors, dirty, touched, isValid, handleChange, handleBlur, handleSubmit } = props;
   return (
-    <form onSubmit={handleSubmit}>
-      {FIELDS[currentForm].map((field): JSX.Element => (
-        <>
-          <label className="a11yHidden" htmlFor={field}>
-            {field.toUpperCase()}
-          </label>
-          <StyledInput
-            id={field}
-            $warning={touched[field] && errors[field]}
-            name={field}
-            placeholder={PLACEHOLDER[field]}
-            type={TYPE[field]}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values[field] || ''}
-          />
-          <StyledFieldError>{touched[field] && errors[field]}</StyledFieldError>
-        </>
-      ))}
+    <StyledAuthContainer>
+      <StyledForm onSubmit={handleSubmit}>
+        {FIELDS[currentForm].map(
+          (field): JSX.Element => (
+            <>
+              <label className="a11yHidden" htmlFor={field}>
+                {field.toUpperCase()}
+              </label>
+              <StyledInput
+                id={field}
+                $warning={touched[field] && errors[field]}
+                name={field}
+                placeholder={PLACEHOLDER[field]}
+                type={TYPE[field]}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values[field] || ''}
+              />
+              <StyledFieldError>{touched[field] && errors[field]}</StyledFieldError>
+            </>
+          ),
+        )}
 
-      <button type="submit" disabled={!dirty || !isValid}>
-        {HEADING[currentForm]}
-      </button>
-    </form>
+        <Button variant="filled" round={true} color="primaryGreen" type="submit" disabled={!dirty || !isValid}>
+          {HEADING[currentForm]}
+        </Button>
+      </StyledForm>
+    </StyledAuthContainer>
   );
 };
 
