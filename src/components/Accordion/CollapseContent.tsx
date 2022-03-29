1acf58ad5ebeb957a7496e348c9ec13be47ec687
jhyj0521 <jhyj3636@gmail.com>
2022-03-30 00:54:32 +0900

    fix: CollapseContent 컴포넌트가 JSX.Element 타입을 반환하도록 수정

--- src/components/Accordion/CollapseContent.tsx
+++ src/components/Accordion/CollapseContent.tsx
@@ -1,30 +1,44 @@
 import { CollapseContentProps } from './Accordion.types';
 import { StyledIngredient, StyledAccordionText } from './Accordion.styled';
 
-export const CollapseContent = ({ type, data }: CollapseContentProps) => {
+export const CollapseContent = ({ type, data }: CollapseContentProps): JSX.Element => {
   switch (type) {
     case 'ingredients':
-      return data.map((ingredient, index) => (
-        <StyledIngredient key={ingredient.name + index}>
-          <span>{ingredient.name}</span>
-          <span>{`${ingredient.amount.toFixed(2)} ${ingredient.unit}`}</span>
-        </StyledIngredient>
-      ));
+      return (
+        <>
+          {data.map((ingredient, index) => (
+            <StyledIngredient key={ingredient.name + index}>
+              <span>{ingredient.name}</span>
+              <span>{`${ingredient.amount.toFixed(2)} ${ingredient.unit}`}</span>
+            </StyledIngredient>
+          ))}
+        </>
+      );
     case 'equipment':
-      return data.map((equipment, index) => <li key={equipment + index}>{equipment}</li>);
+      return (
+        <>
+          {data.map((equipment, index) => (
+            <li key={equipment + index}>{equipment}</li>
+          ))}
+        </>
+      );
     case 'summary':
-      return data
-        .split('. ')
-        .map((text, index, texts) => (
-          <StyledAccordionText key={text + index}>{text + (index < texts.length - 1 ? '.' : '')}</StyledAccordionText>
-        ));
+      return (
+        <>
+          {data.split('. ').map((text, index, texts) => (
+            <StyledAccordionText key={text + index}>{text + (index < texts.length - 1 ? '.' : '')}</StyledAccordionText>
+          ))}
+        </>
+      );
     case 'instructions':
-      return data.map((instruction, index) => (
-        <li key={instruction + index}>
-          <StyledAccordionText>{instruction}</StyledAccordionText>
-        </li>
-      ));
-    default:
-      return null;
+      return (
+        <>
+          {data.map((instruction, index) => (
+            <li key={instruction + index}>
+              <StyledAccordionText>{instruction}</StyledAccordionText>
+            </li>
+          ))}
+        </>
+      );
   }
 };
