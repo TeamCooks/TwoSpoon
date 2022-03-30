cf52a90615adf4d404cf5f68e016885c62ea8b68
hanana1253 <hanana1253@gmail.com>
2022-03-30 14:50:44 +0900

    chore: Error Test 페이지 레이아웃 스타일링
    - 에러페이지에 Empty page 컴포넌트 삽입

--- src/pages/errorTest.tsx
+++ src/pages/errorTest.tsx
@@ -1,21 +1,21 @@
 import { NextPage } from 'next';
-import { Button, Heading } from 'components';
+import { Button, Heading, EmptyPage } from 'components';
 
 const ErrorTest: NextPage = () => {
   return (
-    <>
+    <EmptyPage>
       <Heading as="h2">Normal Page</Heading>
       <Button
         type="button"
         variant="transparent"
         color="primaryGreen"
         onClick={() => {
-          throw new Error('haha');
+          throw new Error('Testing Error!');
         }}
       >
         에러만들기
       </Button>
-    </>
+    </EmptyPage>
   );
 };
 
