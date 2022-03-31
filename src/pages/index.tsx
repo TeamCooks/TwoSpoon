d5d2aad3e5c7617f073a1263925affee6f927c8a
hustle-dev <dlwoabsdk@gmail.com>
2022-03-31 23:07:58 +0900

    fix: Card 코드 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,18 @@ import type { NextPage } from 'next';
 import { useGetRandomRecipeQuery } from 'store/services';
 import Head from 'next/head';
 import Image from 'next/image';
+import { HotRecipes, RandomRecipe } from 'components';
 
 const Home: NextPage = () => {
-  const {
-    data: { recipes },
-    error,
-    isLoading,
-  } = useGetRandomRecipeQuery(1);
-  console.log(recipes);
-  console.log(error);
-  console.log(isLoading);
-
-  return <div>Home</div>;
+  return (
+    <>
+      <Head>
+        <title>Home - HanSpoon</title>
+      </Head>
+      <RandomRecipe />
+      <HotRecipes />
+    </>
+  );
 };
 
 export default Home;
